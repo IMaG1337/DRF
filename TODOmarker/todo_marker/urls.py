6fe2ff6644e9144946d3c7172894ed08fbe0968c
IMaG1337 <007mail.ru96@mail.ru>
2022-02-28 19:48:47 +0300

    fix upper app

--- TODOmarker/todo_marker/urls.py
+++ TODOmarker/todo_marker/urls.py
@@ -1,4 +1,4 @@
-"""TODOmarker URL Configuration
+"""todo_marker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
