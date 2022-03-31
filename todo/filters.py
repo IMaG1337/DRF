859361a7f11f8a75583042b91f6e270ecfb00280
IMaG1337 <007mail.ru96@mail.ru>
2022-03-31 14:25:19 +0300

    todo/filters.py

--- todo/filters.py
+++ todo/filters.py
@@ -1,30 +1,21 @@
 from dataclasses import field
-
 from django_filters import rest_framework as filters
-
-from .models import TODO, Project
-
+from .models import Project, TODO
 
 class ProjectFilter(filters.FilterSet):
-    name = filters.CharFilter(lookup_expr="contains")
+    name = filters.CharFilter(lookup_expr='contains')
+
 
     class Meta:
         model = Project
         fields = ["name"]
 
 
-# class TODOFilter(filters.FilterSet):
-#     project = filters.ModelChoiceFilter(
-#     field_name="project__name",
-#     queryset=Project.objects.all()
-#     )
-
-#     class Meta:
-#         model = TODO
-#         fields = ["project"]
 class TODOFilter(filters.FilterSet):
-    project = filters.CharFilter(field_name="project__name")
-
+    project = filters.CharFilter(
+    field_name="project__name",)
+    
     class Meta:
         model = TODO
         fields = ["project"]
+        
