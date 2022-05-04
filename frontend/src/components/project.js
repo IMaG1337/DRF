e45bd14415cb1705fc52b2a29d06a8854bb9c563
IMaG1337 <007mail.ru96@mail.ru>
2022-05-04 19:38:48 +0300

    post delete

--- frontend/src/components/project.js
+++ frontend/src/components/project.js
@@ -1,45 +1,37 @@
 import React from "react";
 
-const ProjectItem = ({ project }) => {
-    return (<tr>
-
-        <td>
-            {project.id}
-        </td>
-        <td>
-            {project.name}
-        </td>
-        <td>
-            {project.link}
-        </td>
-        <td>
-            {project.users}
-        </td>
-    </tr>)
-}
-
-const ProjectList = ({ projects }) => {
-    return (<table className="fa-user">
+const ProjectItem = ({ project, delete_project }) => {
+    return (
         <tr>
+            <td>{project.id}</td>
+            <td>{project.name}</td>
+            <td>{project.link}</td>
+            <td>{project.users.join(", ")}</td>
+            <td>
+                <button onClick={() => delete_project(project.id)} type="button">
+                    Delete
+                </button>
+            </td>
+        </tr>
+    );
+};
+
+const ProjectList = ({ projects, delete_project }) => {
+    return (
+        <table className="fa-user">
             <thead>
-                <td>
-                    ID
-                </td>
-                <td>
-                    Name
-                </td>
-                <td>
-                    Link
-                </td>
-                <td>
-                    Users
-                </td>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Link</td>
+                <td>Users</td>
             </thead>
             <tbody>
-                {projects.map((project) => <ProjectItem project={project} />)}
+                {projects.map((project) => (
+                    <ProjectItem project={project} delete_project={delete_project} />
+                ))}
             </tbody>
-        </tr>
-    </table>)
-}
+        </table>
+    );
+};
 
 export default ProjectList;
--- frontend/src/components/todo.js
+++ frontend/src/components/todo.js
@@ -1,65 +1,43 @@
 import React from "react";
 
-const TodoIteam = ({ todo }) => {
-    return (<tr>
+const TodoIteam = ({ todo, delete_todo }) => {
+    return (
+        <tr>
+            <td>{todo.id}</td>
+            <td>{todo.text}</td>
+            <td>{todo.create_date}</td>
+            <td>{todo.edit_date}</td>
+            <td>{todo.active.toString()}</td>
+            <td>{todo.project}</td>
+            <td>{todo.user_created}</td>
+            <td>
+                <button onClick={() => { delete_todo(todo.id); todo.active = false }} type="button">
+                    Delete
+                </button>
+            </td>
+        </tr >
+    );
+};
 
-        <td>
-            {todo.id}
-        </td>
-        <td>
-            {todo.text}
-        </td>
-        <td>
-            {todo.createDate}
-        </td>
-        <td>
-            {todo.editDate}
-        </td>
-        <td>
-            {todo.active.toString()}
-        </td>
-        <td>
-            {todo.project}
-        </td>
-        <td>
-            {todo.userCreated}
-        </td>
-    </tr>)
-}
-
-
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, delete_todo }) => {
     return (
         <table className="fa-user">
-            <tr>
-                <thead>
-                    <td>
-                        ID
-                    </td>
-                    <td>
-                        Text
-                    </td>
-                    <td>
-                        Create Date
-                    </td>
-                    <td>
-                        Edit Date
-                    </td>
-                    <td>
-                        Active
-                    </td>
-                    <td>
-                        Project
-                    </td>
-                    <td>
-                        User Created
-                    </td>
-                </thead>
-                <tbody>
-                    {todos.map((todo) => <TodoIteam todo={todo} />)}
-                </tbody>
-            </tr>
-        </table>)
-}
+            <thead>
+                <td>ID</td>
+                <td>Text</td>
+                <td>Create Date</td>
+                <td>Edit Date</td>
+                <td>Active</td>
+                <td>Project</td>
+                <td>User Created</td>
+            </thead>
+            <tbody>
+                {todos.map((todo) => (
+                    <TodoIteam todo={todo} delete_todo={delete_todo} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
 export default TodoList;
